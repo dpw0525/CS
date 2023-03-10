df43b25caf44d0271e350f483215062711cdac3d
Derek Ward <125997543+dpw0525@users.noreply.github.com>
2023-03-09 21:18:32 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,36 +1,42 @@
-﻿//use the whole computer
-using System;
-
-//name of file 
-namespace NumbericTypes
+﻿// whole computer 
+using System; 
+// name of file 
+namespace CountingCharacters
 {
-
-//what program?
-    class rectangle
-    {
-     //visual to others, 
+     class Program
+    { // public then static void main string and args for arguements 
        public static void Main(string[] args)
         {
-            // doubles are numbers, and lists of all tasks for it to find, string will be complete
-            double length;
-            double width;
-            double area;
-            string input;
-
-            //Now, tell console to do that task
-            Console.WriteLine("length: ");
-            input = Console.ReadLine();
-            length = double.Parse(input);
-
-            Console.WriteLine ("width: ");
-            input = Console.ReadLine();
-            width = double.Parse(input); 
-    
-            area = (length * width);
-            Console.WriteLine ("area: " + area);
-            input = Console.ReadLine();
+            // string and the value of it which is a sentence
+            // foreach is a value of a character 
+            const string
+            value = "Time will not slow down when something unpleasant lies ahead!";
+            foreach (char a in value)
+            {
+                //tell the console to write the line 
+                Console.WriteLine (a);
+            }
+                   Dictionary<char, int> diclist = new Dictionary< char, int> ();
+                Console.WriteLine("Enter string");
+                
+                foreach (var chr in Console.ReadLine())
+          {
+            int count = 0;
+            if(diclist.ContainsKey(chr))
+            {
+              count = diclist[chr];
+            
+            } 
+            diclist[chr] = count +1; 
+                }
+                foreach (KeyValuePair<char, int> pair in diclist)
+                {
+                    Console.WriteLine(pair.Key + " = " + pair.Value);
+                }
+            Console.ReadLine();
         }
-
-    }   
-
+    }    
 }
+// PLEASE READ NOTE BELOW 
+// when prompt to enter string, you'll need to enter the full STRING: 
+// "Time will not slow down when something unpleasant lies ahead!"
